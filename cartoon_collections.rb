99163b6ef7e0bf6371857af478fc4bebb7ab7068
Mike Shippy <mikeship04@gmail.com>
2022-07-22 13:34:02 -0700

    commit tree b7798ecd748fb8f0c763f551c9ccfc82d24e769f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.map.with_index(1) do |dwarf, i|
+    puts "#{i}.  #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planateer_array)
+  planateer_array.map {|call| call.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_array)
+  calls_array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_array.find do |is_cheese|
+    cheese_types.include?(is_cheese)
+  end
 end
